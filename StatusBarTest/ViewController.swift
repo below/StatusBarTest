448c0301de26a0e7640307f802a0d8bd0ddad0f9
Alexander von Below <Alex@vonBelow.Com>
2016-06-29 14:59:48 +0200

    Works with Navigation Controller

--- StatusBarTest/ViewController.swift
+++ StatusBarTest/ViewController.swift
@@ -9,14 +9,5 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setNeedsStatusBarAppearanceUpdate()
-    }
-
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return .LightContent
-    }
 }
 
